0cbbf8969bc8aa8435af5403c1191da7fbfc6190
Metal Beast Legend <junalenovonew@gmail.com>
2025-07-09 22:04:54 +0700

    a

--- utama.php
+++ utama.php
@@ -54,6 +54,7 @@ function perpus_modulku() {
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Transaksi</a>
                         <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="admin.php?page=perpus_utama&panggil=kunjungan.php">Entry Kunjungan</a></li>
                             <li><a class="dropdown-item" href="admin.php?page=perpus_utama&panggil=peminjaman.php">Entry Peminjaman</a></li>
                             <li><a class="dropdown-item" href="admin.php?page=perpus_utama&panggil=pengembalian.php">Entry Pengembalian</a></li>
                         </ul>
@@ -63,6 +64,7 @@ function perpus_modulku() {
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Cetak</a>
                         <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="admin.php?page=perpus_utama&panggil=lapkun.php">Lihat Laporan Kunjungan</a></li>
                             <li><a class="dropdown-item" href="admin.php?page=perpus_utama&panggil=laporan_peminjaman.php">Cetak Laporan Peminjaman</a></li>
                             <li><a class="dropdown-item" href="admin.php?page=perpus_utama&panggil=laporan_pengembalian.php">Cetak Laporan Pengembalian</a></li>
                         </ul>
